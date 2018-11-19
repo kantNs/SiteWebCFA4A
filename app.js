c19be3d2adadaf1e15415e71ac56f41ce3e198da
kantNs <37747318+kantNs@users.noreply.github.com>
2018-11-19 20:44:28 +0100

    Cookie update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(session({
 	secret: 'keyboard cat',
 	resave:false,
 	saveUninitialized:true,
-	cookie:{secure:true}
+	cookie:{secure:false, maxAge: 24 * 60 * 60 * 1000},
 	}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
